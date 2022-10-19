e4be59f51fd48709820118701021759d23d8fcfb
muchachooos <odnorogg@bk.ru>
2022-10-19 15:41:25 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"os"
 )
@@ -14,7 +15,22 @@ func main() {
 	router.GET("/3page_with_text", MyHandler3)
 	router.GET("/4page_with_text", MyHandler4)
 
+	router.GET("/login_page", pageHandler)
+	router.POST("/login", LoginHandler)
+
 	router.Run("localhost:8080")
+}
+func pageHandler(context *gin.Context) {
+	html, _ := os.ReadFile("./html/page_with_authorization.html")
+	context.Writer.Write(html)
+}
+
+func LoginHandler(context *gin.Context) {
+	user, _ := context.GetQuery("username")
+	pas, _ := context.GetQuery("password")
+	fmt.Println(user, pas)
+	context.Writer.WriteString("OK")
+	//context.Writer.Write([]byte("OK"))
 }
 
 func MyHandler(context *gin.Context) {
@@ -25,6 +41,8 @@ func MyHandler(context *gin.Context) {
 func MyHandler1(context *gin.Context) {
 	html, _ := os.ReadFile("./html/1page_with_text.html")
 	context.Writer.Write(html)
+	//t, _ := context.GetQuery("some_key")
+	//context.Writer.WriteString("Ebal4" + t + " Ebala")
 }
 
 func MyHandler2(context *gin.Context) {
