70671bd7cbc8ca6eb2b18e8f3fbcdad579361809
muchachooos <odnorogg@bk.ru>
2022-10-24 20:04:06 +0400

    Create sort.go

---
+++ sort/sort.go
@@ -0,0 +1,12 @@
+package sort
+
+func BubbleSort(slice []int) []int {
+	for i := 0; i < len(slice)-1; i++ {
+		for j := 0; j < len(slice)-i-1; j++ {
+			if slice[j] > slice[j+1] {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+	return slice
+}
