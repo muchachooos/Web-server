4ac3341e2833e43e86326615dd3df571645dc033
muchachooos <odnorogg@bk.ru>
2022-11-03 19:58:16 +0400

    Update main.go

--- main.go
+++ main.go
@@ -22,90 +22,27 @@ func main() {
 	}
 
 	router.GET("", handler.MyHandler)
+
 	router.GET("/chop_suey_page", handler.MyHandler1)
 	router.GET("/chop_suey_rus_page", handler.MyHandler2)
+
 	router.GET("/toxicity_page", handler.MyHandler3)
 	router.GET("/toxicity_rus_page", handler.MyHandler4)
 
 	router.GET("/login_page", handler.PageLogHandler)
 	router.GET("/login", server.LoginHandler)
+
 	router.GET("/registration_page", handler.PageRegHandler)
 	router.GET("/registration", server.RegistrationHandler)
 
-	router.GET("/sort_slice_page", handler.SortHandler)
-	router.GET("/sort_slice", handler.ArraySortHandler)
+	router.GET("/delete_user_page", handler.PageDelHandler)
+	router.GET("/delete_user", server.DeleteHandler)
+
+	router.GET("/change_pass_page", handler.PageChangeHandler)
+	router.GET("/change_pass", server.ChangeHandler)
+
+	router.GET("/sort_slice_page", handler.PageSortHandler)
+	router.GET("/sort_slice", handler.SortHandler)
 
 	router.Run("localhost:8080")
 }
-
-/*
-func RegistrationHandler(context *gin.Context) {
-	user, ok := context.GetQuery("username") //Достаём Query-параметр(user = key(username))
-	if user == "" || !ok {                   //ok == false; Поверка на пустые значения
-		context.Writer.WriteString("No username")
-		return
-	}
-
-	pass, ok := context.GetQuery("password") //Достаём Query-параметр(pass = key(password))
-	if pass == "" || !ok {                   //ok == false; Поверка на пустые значения
-		context.Writer.WriteString("No password")
-		return
-	}
-
-	for i := range dataBase {
-		if user == dataBase[i].login {
-			context.Writer.WriteString("This login or password already exist. Try again")
-			return
-		}
-		if pass == dataBase[i].password {
-			context.Writer.WriteString("This password already exist. Try again")
-			return
-		}
-	}
-
-	dataBase = append(dataBase, User{user, pass})
-
-	context.Writer.WriteString("Welcome to the club Body")
-}
-*/
-
-//a, ok := context.GetQuery("username") //Достаём Query-параметр(user = key(username))
-//if a == "" || !ok {                   //ok == false; Поверка на пустые значения
-//	context.Writer.WriteString("No username")
-//	return
-//}
-//
-//b, ok := context.GetQuery("password") //Достаём Query-параметр(pass = key(password))
-//if b == "" || !ok {                   //ok == false; Поверка на пустые значения
-//	context.Writer.WriteString("No password")
-//	return
-//}
-
-//log
-//
-//user, ok := context.GetQuery("username")
-//if user == "" || !ok { //ok == false; Поверка на пустые значения
-//	context.Writer.WriteString("No username")
-//	return
-//}
-//
-//pass, ok := context.GetQuery("password")
-//if pass == "" || !ok { //ok == false; Поверка на пустые значения
-//	context.Writer.WriteString("No password")
-//	return
-//}
-//
-//for i := range dataBaseArr {
-//	if user == dataBaseArr[i].login && pass == dataBaseArr[i].password {
-//		fmt.Println(user, pass)
-//		context.Writer.WriteString("Welcome to the club Body")
-//		return
-//	}
-//}
-
-//type User struct {
-//	login    string
-//	password string
-//}
-//
-//var dataBaseArr []User
