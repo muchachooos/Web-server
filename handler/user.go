9497639900bdccc6ae3118378fbc743f9ca98cbe
muchachooos <odnorogg@bk.ru>
2022-12-03 17:21:37 +0400

    Create user.go

---
+++ handler/user.go
@@ -0,0 +1,76 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) LoginHandler(context *gin.Context) {
+
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok { //ok == false; Поверка на пустые значения
+		context.Writer.WriteString("No login")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok { //ok == false; Поверка на пустые значения
+		context.Writer.WriteString("No password")
+		return
+	}
+
+	resultTable, err := s.Storage.GetUserInDB(log, pass)
+	if err != nil {
+		context.Status(500)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
+	if len(resultTable) == 0 {
+		context.Status(404)
+		context.Writer.WriteString("Wrong login or password. Try again")
+		return
+	}
+
+	jsonInByte, err := json.Marshal(resultTable)
+	if err != nil {
+		context.Writer.WriteString("json creating error")
+		return
+	}
+
+	context.Writer.Write(jsonInByte)
+}
+
+func (s *Server) RegistrationHandler(context *gin.Context) {
+
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok {
+		context.Writer.WriteString("No login")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok {
+		context.Writer.WriteString("No password")
+		return
+	}
+
+	context.Writer.WriteString("Welcome to the club Body")
+}
+
+func (s *Server) DeleteHandler(context *gin.Context) {
+
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok {
+		context.Writer.WriteString("No login")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok {
+		context.Writer.WriteString("No password")
+		return
+	}
+
+	context.Writer.WriteString("Welcome to the club Body")
+}
