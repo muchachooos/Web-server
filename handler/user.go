1d15129dc33d763cd0541d12db2141eb3b6dca3e
muchachooos <odnorogg@bk.ru>
2022-12-04 16:44:51 +0400

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -19,7 +19,7 @@ func (s *Server) LoginHandler(context *gin.Context) {
 		return
 	}
 
-	resultTable, err := s.Storage.GetUserInDB(log, pass)
+	resultTable, err := s.Storage.GetLoginUserInDB(log, pass)
 	if err != nil {
 		context.Status(500)
 		context.Writer.WriteString("Something went wrong. Try again")
@@ -55,6 +55,13 @@ func (s *Server) RegistrationHandler(context *gin.Context) {
 		return
 	}
 
+	err := s.Storage.RegistrationUserInBD(log, pass)
+	if err != nil {
+		context.Status(500)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
 	context.Writer.WriteString("Welcome to the club Body")
 }
 
@@ -69,6 +76,58 @@ func (s *Server) DeleteHandler(context *gin.Context) {
 	pass, ok := context.GetQuery("password")
 	if pass == "" || !ok {
 		context.Writer.WriteString("No password")
+		return
+	}
+
+	isDeleted, err := s.Storage.DeleteUserFromDB(log, pass)
+	if err != nil {
+		context.Status(500)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
+	if isDeleted == false {
+		context.Writer.WriteString("Something went wrong")
+		return
+	}
+
+	context.Writer.WriteString("Welcome to the club Body")
+}
+
+func (s *Server) ChangeHandler(context *gin.Context) {
+
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok {
+		context.Writer.WriteString("No login")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok {
+		context.Writer.WriteString("No password")
+		return
+	}
+
+	newPass, ok := context.GetQuery("newPassword")
+	if newPass == "" || !ok {
+		context.Writer.WriteString("No new password")
+		return
+	}
+
+	if pass == newPass {
+		context.Writer.WriteString("Incorrect new password")
+		return
+	}
+
+	isChanged, err := s.Storage.ChangePassUserInDB(log, pass, newPass)
+	if err != nil {
+		context.Status(500)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
+	if isChanged == false {
+		context.Writer.WriteString("Something went wrong")
 		return
 	}
 
